48cf805ddd22b21b7460f3c5a02934e4473cd183
Paul Leelaviwatana <apaulture@gmail.com>
2023-04-14 12:04:35 -0700

    Browsable API with bootstrap css; #4: Completed updating our serializer, next: Adding required permissions

--- restapp/views.py
+++ restapp/views.py
@@ -4,17 +4,23 @@ from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 
 from django.http import Http404
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 from restapp.models import Snippet
 from restapp.serializers import SnippetSerializer
+from restapp.serializers import *
 
 class SnippetList(APIView):
     """
     List all code snippets, or create a new snippet.
     """
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
+
     def get(self, request, format = None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many = True)
@@ -24,7 +30,7 @@ class SnippetList(APIView):
         serializer = SnippetSerializer(data = request.data)
 
         if serializer.is_valid():
-            serializer.save()
+            serializer.save() # perform_create is called here
             return Response(serializer.data, status = status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
@@ -58,3 +64,11 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
